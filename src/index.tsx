4919f2c31ab730e4d7a421c9636d7046919e38de
turnintolight <sunjinhua266@163.com>
2022-05-02 10:28:56 +0800

    build: package.json adjust

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 
 export { Button } from './components/Button'
 
